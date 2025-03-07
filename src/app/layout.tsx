3d516d2cf2b6f967f21fd5daf819aa9687e752f0
nettobruno <bruno.rosa@grupoboticario.com.br>
2025-03-07 20:12:16 -0300

    adiciona e utiliza favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
-import "../styles/globals.css"
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Bruno Netto - Desenvolvedor Front End",
-  description: "Explore meus projetos, habilidades e experiência como desenvolvedor Front End. Veja como posso ajudar a transformar suas ideias em soluções digitais.",
-  keywords: "desenvolvedor web, portfólio, projetos, React, Next.js, JavaScript, TypeScript, CSS-in-JS, Styled Components, Stitches, Design System, Git, Github",
+  description:
+    "Explore meus projetos, habilidades e experiência como desenvolvedor Front End. Veja como posso ajudar a transformar suas ideias em soluções digitais.",
+  keywords:
+    "desenvolvedor web, portfólio, projetos, React, Next.js, JavaScript, TypeScript, CSS-in-JS, Styled Components, Stitches, Design System, Git, Github",
   robots: "index, follow",
+  icons: {
+    icon: "/images/favicon.ico",
+  },
 };
-
 
 export default function RootLayout({
   children,
